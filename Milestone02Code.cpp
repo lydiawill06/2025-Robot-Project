1fbfdafca1669b8edd1cd0e80cc2dd7e355265df
calebfinkler <calebfinkler@outlook.com>
2025-03-03 18:26:42 -0500

    Update Milestone02Code.cpp

--- Milestone02Code.cpp
+++ Milestone02Code.cpp
@@ -3,6 +3,11 @@
 #include <FEHUtility.h>
 #include <FEHMotor.h>
 
+FEHMotor left_motor(FEHMotor::Motor1, 9.0);
+FEHMotor right_motor(FEHMotor::Motor2, 9.0);
+DigitalEncoder right_encoder(FEHIO::P1_5);
+DigitalEncoder left_encoder(FEHIO::P1_4);
+
 void move_forward(int inches) //using encoders
 {
     int percent = 25;
@@ -12,8 +17,8 @@ void move_forward(int inches) //using encoders
     left_encoder.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(percent);
+    right_motor.SetPercent(-percent);
+    left_motor.SetPercent(-percent);
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
@@ -25,19 +30,36 @@ void move_forward(int inches) //using encoders
 }
 
 void turn_left(int degree){
+    //Reset Counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+
+
     int percent = 25;
-    int counts = degree * 2.17888888889;
-    right_motor.SetPercent((percent));
-    left_motor.SetPercent(-1 * (percent));
+    int counts = degree * 2.00000000000;
+    right_motor.SetPercent(-1*(percent));
+    left_motor.SetPercent((percent));
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
 }
 
 void turn_right(int degree){
+    //Reset Counts
+    right_encoder.ResetCounts();
+    left_encoder.ResetCounts();
+    
     int percent = 25;
-    int counts = degree * 2.17888888889;
-    right_motor.SetPercent((-1* (percent)));
-    left_motor.SetPercent(percent);
+    int counts = degree * 2.00000000000;
+    right_motor.SetPercent(percent);
+    left_motor.SetPercent(-1* (percent));
     while((left_encoder.Counts() + right_encoder.Counts()) / 2. < counts);
+
+    //Turn off motors
+    right_motor.Stop();
+    left_motor.Stop();
 } 
 
 void check_light(){
@@ -49,7 +71,7 @@ void check_light(){
   turn_right(90);
   move_forward(7);
 
-Sleep(10);
+Sleep(10.0);
   
   turn_right(90);
   move_forward(2);
