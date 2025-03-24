8533faf48469fad84e0703a1e84c68c6f793aeec
calebfinkler <calebfinkler@outlook.com>
2025-03-24 19:43:51 -0400

    Update Servo_Motor_Test.cpp

--- Servo_Motor_Test.cpp
+++ Servo_Motor_Test.cpp
@@ -15,10 +15,19 @@ DigitalEncoder left_encoder(FEHIO::P1_4);
 AnalogInputPin CDS_Sensor(FEHIO::P2_1);
 FEHServo Arm_Servo(FEHServo::Servo3);
 
-
 int main()
 {
-
-  //calibrate the servo
-  Arm_Servo.TouchCalibrate();
+//enter the minimum and maximum servo values
+    //from the .TouchCalibrate() function
+    Arm_Servo.SetMin(863);
+    Arm_Servo.SetMax(2410);
+ 
+    //set the servo angle to it's initial angle
+    Arm_Servo.SetDegree(90.);
+     
+    //wait 5 seconds
+    Sleep(5.0);
+ 
+    //move the arm to next angle
+    Arm_Servo.SetDegree(114.5);
 }
